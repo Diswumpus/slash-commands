416683daf314654cbf89b53578a02c178a0ca511
TurtlePaw <turtlesimsyt@gmail.com>
2021-09-10 15:45:26 -0400

    Add codeblocks

--- events/guild.js
+++ events/guild.js
@@ -23,14 +23,14 @@ module.exports = {
         .setColor(`GREEN`)
         .setAuthor(client.user.username, client.user.displayAvatarURL())
         .setDescription(`**Joined a server**
-                Guild: ${guild.name} (${guild.id})
-                Users: ${guild.memberCount}
-                Channels: ${guild.channels.cache.size}
-                NSFW: ${guild.nsfwLevel}
-                Owner: ${(await guild.fetchOwner()).user.tag} (${(await guild.fetchOwner()).user.id})
-                Owner ID: (${(await guild.fetchOwner()).user.id})
+                Guild: ${guild.name} (\`${guild.id}\`)
+                Users: \`${guild.memberCount}\`
+                Channels: \`${guild.channels.cache.size}\`
+                NSFW: \`${guild.nsfwLevel}\`
+                Owner: ${(await guild.fetchOwner()).user.tag} (\`${(await guild.fetchOwner()).user.id}\`)
+                Owner ID: (\`${(await guild.fetchOwner()).user.id}\`)
                 Invite: ${inv.url}`)
-        .setFooter(`Guild ID: ${guild.id}`)
+        .setFooter(`Guild ID: \`${guild.id}\``)
         .setThumbnail(guild.iconURL())
         .setTimestamp()
 
