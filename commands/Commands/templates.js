38a7bc044075a3c0687eca417afc453ab0706c70
TurtlePaw <turtlesimsyt@gmail.com>
2022-01-16 21:25:57 -0500

    Add templates

--- commands/Commands/templates.js
+++ commands/Commands/templates.js
@@ -4,6 +4,7 @@ const { color } = require("../../color.json");
 const template = require("../../models/template");
 const { templates } = require("../../config.json").logs;
 const du = require("discord.js-util");
+const { errorMessage } = require("../../functions")
 
 const Config = module.exports = {
     name: "templates",
@@ -44,6 +45,12 @@ module.exports.execute = async (client, interaction) => {
     const subcommand = interaction.options.getSubcommand();
 
     if (subcommand === "create") {
+        const fetch = await template.findOne({
+            title: interaction.options.getString("title")
+        });
+
+        if(fetch) return await errorMessage(`There's already a template with this title!`, interaction, "REPLY", true);
+
         await interaction.reply({
             embeds: [
                 new Discord.MessageEmbed()
@@ -73,20 +80,24 @@ module.exports.execute = async (client, interaction) => {
                     .addField(`${client.botEmojis.user_add || "\`Emojis Loading\`"} User:`, `**Tag:** ${user.tag || "No user added"}\n**Username:** ${user.username || "No user added"}\n**ID:** \`${user.id || "No user added"}\``)
                     .setColor(color)
 
-                const ms = await webhook.send({ embeds: [embed], components: [
-                    { type: 1, components: [
-                        new Discord.MessageButton()
-                        .setStyle("SECONDARY")
-                        .setEmoji(client.botEmojis.b_check.show)
-                        .setLabel("Approve")
-                        .setCustomId("ta"),
-                        new Discord.MessageButton()
-                        .setStyle("SECONDARY")
-                        .setEmoji(client.botEmojis.b_xmark.show)
-                        .setLabel("Deny")
-                        .setCustomId("td")
-                    ]}
-                ]})
+                const ms = await webhook.send({
+                    embeds: [embed], components: [
+                        {
+                            type: 1, components: [
+                                new Discord.MessageButton()
+                                    .setStyle("SECONDARY")
+                                    .setEmoji(client.botEmojis.b_check.show)
+                                    .setLabel("Approve")
+                                    .setCustomId("ta"),
+                                new Discord.MessageButton()
+                                    .setStyle("SECONDARY")
+                                    .setEmoji(client.botEmojis.b_xmark.show)
+                                    .setLabel("Deny")
+                                    .setCustomId("td")
+                            ]
+                        }
+                    ]
+                })
 
                 await new template({
                     userID: interaction.user.id,
@@ -98,38 +109,212 @@ module.exports.execute = async (client, interaction) => {
                 }).save().catch(e => console.log(e));
             })
     } else if (subcommand === "list") {
-        const Fetch = await template.find({ approved: true });
-        const embeds = [];
+        /**
+         * @type {Discord.Message}
+         */
+        const Message = await interaction.reply({
+            embeds: [
+                new Discord.MessageEmbed()
+                    .setColor(color)
+                    .setDescription(`Type of pack:`)
+            ],
+            components: [
+                {
+                    type: 1,
+                    components: [
+                        new Discord.MessageButton()
+                            .setCustomId("verified")
+                            .setStyle("SECONDARY")
+                            .setLabel("Official Templates")
+                            .setEmoji("<:bot_crown:932401932058910770>"),
+                        new Discord.MessageButton()
+                            .setLabel("User Templates")
+                            .setCustomId("users")
+                            .setStyle("SECONDARY")
+                            .setEmoji("<:bot_1:932401951633715241>")
+                    ]
+                }
+            ],
+            fetchReply: true
+        });
 
-        let i = 1;
-        let i2 = 0;
+        const collector = Message.createMessageComponentCollector({
+            filter: i => i.user.id === interaction.user.id
+        });
 
-        let text = "";
-        let textLengths = []
-        let pages = []
-        let currentPage = "";
-        let msgCount = 0;
-        for (const c of Fetch) {
-            let content = `**Title:** ${c.title}\n**Description:** ${c.description}\n**User:** [\`${client.users.cache.get(c.userID).tag}\`](discord://${client.application.id}/users/${c.userID})\n\n`
-            let textToAdd = content
-            currentPage += textToAdd;
-            msgCount++;
-            if (msgCount % 10 == 0) {
-                pages.push(currentPage)
-                currentPage = []
+        collector.on("collect", async i => {
+            if (!i.isButton()) return;
+            if(i.customId != "verified" && i.customId != "users") return;
+            let Fetch;
+            if (i.customId == "verified") {
+                Fetch = await template.find({ approved: true, userID: "820465204411236362" });
+            } else if (i.customId == "users") {
+                Fetch = await template.find({ approved: true });
             }
-        }
-        if (currentPage.length > 0) pages.push(currentPage)
-        let ii = 0
-        for (const textt of pages) {
-            embeds.push(new Discord.MessageEmbed().setColor("BLURPLE").setAuthor(interaction.user.tag, interaction.user.displayAvatarURL()).setDescription(textt))
-        }
 
-        await new du.pages()
-            .setEmojis(client.botEmojis.leave.show, client.botEmojis.join.show)
-            .setInteraction(interaction)
-            .setPages(embeds)
-            .send({ ephemeral: true });
+            const embeds = [];
+
+            let text = "";
+            let textLengths = []
+            let pages = []
+            let currentPage = [];
+            let msgCount = 0;
+            const AllFetches = [];
+            for (const c of Fetch) {
+                if(!c || !c.title) continue
+                AllFetches.push(c);
+                /**
+                 * @type {Discord.MessageSelectOptionData}
+                 */
+                let content = {
+                    value: c.title.toLowerCase(),
+                    label: c.title,
+                    description: c.description,
+                    emoji: "<:messages:863464329667411998>"
+                }
+
+                currentPage.push(content);
+                msgCount++;
+                if (msgCount % 10 == 0) {
+                    pages.push(currentPage)
+                    currentPage = []
+                }
+            }
+            if (currentPage.length > 0) pages.push(currentPage)
+            let ii = 0
+            for (const textt of pages) {
+                embeds.push(new Discord.MessageSelectMenu()
+                    .setOptions(textt)
+                    .setPlaceholder("Select a template")
+                    .setCustomId("template_choose")
+                )
+            }
+
+            let count = 0;
+
+            function PageCount(){
+                return new Discord.MessageButton()
+                .setStyle("PRIMARY")
+                .setLabel(`${count+1} of ${embeds.length}`)
+                .setCustomId("page_count")
+                .setDisabled(true)
+            }
+
+            function checkDisabled(){
+                return embeds.length > 1 ? false : true;
+            }
+
+            function IsVerified(userID){
+                if(userID === "820465204411236362") return true
+                else return false;
+            }
+            
+            await i.update({
+                components: [
+                    {
+                        type: 1,
+                        components: [
+                            new Discord.MessageButton()
+                            .setCustomId("sm<")
+                            .setEmoji("<:left:932440776158375946>")
+                            .setStyle("SECONDARY")
+                            .setDisabled(checkDisabled()),
+                            PageCount(),
+                            new Discord.MessageButton()
+                            .setCustomId("sm>")
+                            .setStyle("SECONDARY")
+                            .setEmoji("<:right:932440776217071676>")
+                            .setDisabled(checkDisabled()),
+                        ]
+                    },
+                    {
+                        type: 1,
+                        components: [
+                            embeds[0]
+                        ]
+                    }
+                ],
+            });
+
+            const collect = Message.createMessageComponentCollector({
+                filter: i => i.user.id === interaction.user.id
+            });
+
+            collect.on("collect", async i2 => {
+                if(i2.customId == "sm>"){
+                    count++
+                    
+                    await i2.update({
+                        components: [
+                            {
+                                type: 1,
+                                components: [
+                                    new Discord.MessageButton()
+                                    .setLabel(">")
+                                    .setCustomId("sm>")
+                                    .setDisabled(checkDisabled())
+                                    .setStyle("SECONDARY"),
+                                    PageCount(),
+                                    new Discord.MessageButton()
+                                    .setLabel("<")
+                                    .setCustomId("sm<")
+                                    .setDisabled(checkDisabled())
+                                    .setStyle("SECONDARY")
+                                ]
+                            },
+                            {
+                                type: 1,
+                                components: [
+                                    embeds[count]
+                                ]
+                            }
+                        ]
+                    });
+                } else if(i2.customId == "sm<"){
+                    count--
+                    
+                    await i2.update({
+                        components: [
+                            {
+                                type: 1,
+                                components: [
+                                    new Discord.MessageButton()
+                                    .setLabel(">")
+                                    .setCustomId("sm>")
+                                    .setDisabled(checkDisabled())
+                                    .setStyle("SECONDARY"),
+                                    PageCount(),
+                                    new Discord.MessageButton()
+                                    .setLabel("<")
+                                    .setCustomId("sm<")
+                                    .setStyle("SECONDARY")
+                                    .setDisabled(checkDisabled())
+                                ]
+                            },
+                            {
+                                type: 1,
+                                components: [
+                                    embeds[count]
+                                ]
+                            }
+                        ]
+                    });
+                } else {
+                    const selected = AllFetches.find(e => e.title.toLowerCase() == i2.values[0]);
+
+                    await i2.reply({
+                        embeds: [
+                            new Discord.MessageEmbed()
+                            .setTitle(`${IsVerified(selected.userID) ? "<:bot_crown:932401932058910770>" : "<:bot_1:932401951633715241>"} ${selected.title}`)
+                            .setDescription(`> ${selected.description}`)
+                            .setColor("BLURPLE")
+                            .addField(`<:BotDeveloper:911666363817414706> Code:`, Discord.Formatters.codeBlock("js", selected.code))
+                        ],
+                        ephemeral: true
+                    });
+                }
+            })
+        });
     } else if (subcommand === "report") {
 
     }
